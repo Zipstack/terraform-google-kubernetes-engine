d1ec170c112b1ecae364f6bfa305ea3aed7fa2a0
Ritwik G <100672805+ritwik-g@users.noreply.github.com>
2025-08-06 19:29:10 +0000

    Removed duplicate

--- modules/beta-private-cluster/cluster.tf
+++ modules/beta-private-cluster/cluster.tf
@@ -116,7 +116,6 @@ resource "google_container_cluster" "primary" {
       for_each = var.cluster_autoscaling.enabled ? [1] : []
 
       content {
-        min_cpu_platform = "AMD Milan"
         service_account  = local.service_account
         oauth_scopes     = local.node_pools_oauth_scopes["all"]
 
